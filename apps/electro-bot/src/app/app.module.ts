16143e089c4ecfeb857c38d36edd33ee16de1fa2
0mniGod <kobrakq1993@gmail.com>
2025-10-22 17:33:54 +0300

    Update app.module.ts

--- apps/electro-bot/src/app/app.module.ts
+++ apps/electro-bot/src/app/app.module.ts
@@ -1,25 +1,21 @@
+// ----- ЦЕЙ КОД "ПІСЛЯ" (ВСТАВТЕ ЦЕ) -----
 import { BotModule } from '@electrobot/bot';
 import { ElectricityAvailabilityModule } from '@electrobot/electricity-availability';
 import { Module } from '@nestjs/common';
-//import { ScheduleModule } from '@nestjs/schedule';
 import { KnexModule } from 'nestjs-knex';
 import { CronService } from './cron.service';
+// KyivElectricScheduleModule ТА ScheduleModule ВИДАЛЕНІ - ВОНИ БУЛИ ЗЛАМАНІ
 
 @Module({
   imports: [
-    ElectricityAvailabilityModule,
+    ElectricityAvailabilityModule, // Це модуль, який пінгує ваш IP
     BotModule,
-//    ScheduleModule.forRoot(),
+    // KyivElectricScheduleModule ТА ScheduleModule ВИДАЛЕНІ
     KnexModule.forRoot({
       config: {
         client: 'pg',
-        connection: {
-          host: process.env.DB_HOST,
-          port: Number(process.env.DB_PORT),
-          user: process.env.DB_USER,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME,
-        },
+        // Ми замінили старий об'єкт connection на цей рядок:
+        connection: process.env.DATABASE_URL,
       },
     }),
   ],
