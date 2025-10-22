82eecf29efdc1f7ad0b2e1796cc96e359f2ed0a5
0mniGod <kobrakq1993@gmail.com>
2025-10-22 18:21:22 +0300

    Update index.ts

--- libs/electricity-availability/src/index.ts
+++ libs/electricity-availability/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/electricity-availability.service';
 export * from './lib/kyiv-electricstatus-schedule.service';
 export * from './lib/electricity-availability.module';
 export * from './lib/history-item.type';
+export * from './lib/electricity.repository'; // <-- ДОДАЙТЕ ЦЕЙ РЯДОК
