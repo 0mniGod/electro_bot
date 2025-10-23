54bbc8f073a9d4b4c7203a7168a245023a25f04b
0mniGod <kobrakq1993@gmail.com>
2025-10-23 20:28:45 +0300

    Update electricity-availability.module.ts

--- libs/electricity-availability/src/lib/electricity-availability.module.ts
+++ libs/electricity-availability/src/lib/electricity-availability.module.ts
@@ -1,15 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common'; // <-- ДОДАНО forwardRef
 import { ElectricityAvailabilityService } from './electricity-availability.service';
 import { HttpModule } from '@nestjs/axios';
 import { ElectricityRepository } from './electricity.repository';
 import { ScheduleModule } from '@nestjs/schedule';
-import { PlaceRepoModule } from '@electrobot/place-repo'; // <-- ДОДАНО ЦЕЙ ІМПОРТ
+import { PlaceRepoModule } from '@electrobot/place-repo';
+import { BotModule } from '@electrobot/bot'; // Імпорт залишається
 
 @Module({
   imports: [
     HttpModule,
     ScheduleModule.forRoot(),
-    PlaceRepoModule, // <-- ДОДАНО ЦЕЙ МОДУЛЬ
+    PlaceRepoModule,
+    forwardRef(() => BotModule), // <-- ВИПРАВЛЕНО
   ],
   providers: [ElectricityAvailabilityService, ElectricityRepository],
   exports: [ElectricityAvailabilityService],
