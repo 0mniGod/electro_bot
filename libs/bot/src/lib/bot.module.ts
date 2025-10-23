ef36dbfbe351cbd2ecd5d386c5e3634b8b19a3b3
0mniGod <kobrakq1993@gmail.com>
2025-10-23 20:14:20 +0300

    Update bot.module.ts

--- libs/bot/src/lib/bot.module.ts
+++ libs/bot/src/lib/bot.module.ts
@@ -1,11 +1,15 @@
-import { ElectricityAvailabilityModule } from '@electrobot/electricity-availability';
+import { Module } from '@nestjs/common';
 import { PlaceRepoModule } from '@electrobot/place-repo';
 import { UserRepoModule } from '@electrobot/user-repo';
-import { Module } from '@nestjs/common';
 import { NotificationBotService } from './notification-bot.service';
+import { ElectricityAvailabilityModule } from '@electrobot/electricity-availability'; // <-- ДОДАЙТЕ ЦЕЙ ІМПОРТ
 
 @Module({
-  imports: [ElectricityAvailabilityModule, UserRepoModule, PlaceRepoModule],
+  imports: [
+    UserRepoModule,
+    PlaceRepoModule,
+    ElectricityAvailabilityModule, // <-- ДОДАЙТЕ ЦЕЙ МОДУЛЬ
+  ],
   providers: [NotificationBotService],
   exports: [NotificationBotService],
 })
