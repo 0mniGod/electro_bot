7ccef48f99ade5a2554fdc9cd74e2d24179e60da
0mniGod <kobrakq1993@gmail.com>
2025-10-23 18:42:26 +0300

    Update electricity-availability.module.ts

--- libs/electricity-availability/src/lib/electricity-availability.module.ts
+++ libs/electricity-availability/src/lib/electricity-availability.module.ts
@@ -1,17 +1,13 @@
-import { ElectricityRepoModule } from '@electrobot/electricity-repo';
-import { PlaceRepoModule } from '@electrobot/place-repo';
 import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
 import { ElectricityAvailabilityService } from './electricity-availability.service';
-//import { KyivElectricstatusScheduleService } from './kyiv-electricstatus-schedule.service';
+import { HttpModule } from '@nestjs/axios';
+// KyivElectricstatusScheduleService та ScheduleModule видалені
+import { ElectricityRepository } from './electricity.repository'; // Імпортуємо репозиторій
+import { ScheduleModule } from '@nestjs/schedule'; // Повертаємо ScheduleModule
 
 @Module({
-  imports: [HttpModule, ElectricityRepoModule, PlaceRepoModule],
-  controllers: [],
-  providers: [
-    ElectricityAvailabilityService,
-//    KyivElectricstatusScheduleService,
-  ],
-  exports: [ElectricityAvailabilityService]//, KyivElectricstatusScheduleService],
+  imports: [HttpModule, ScheduleModule.forRoot()], // Повертаємо ScheduleModule.forRoot()
+  providers: [ElectricityAvailabilityService, ElectricityRepository], // <-- Додаємо ElectricityRepository
+  exports: [ElectricityAvailabilityService],
 })
 export class ElectricityAvailabilityModule {}
